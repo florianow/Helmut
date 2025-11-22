53900575978fd2327b42767b1231499aced9ae9d
Florian Nowarre <fnowarre@meshcloud.io>
2025-11-22 16:23:45 +0100

    chore: fix tests

---
+++ app/src/test/java/com/helmut/ExampleUnitTest.kt
@@ -0,0 +1,16 @@
+package com.helmut
+
+import org.junit.Test
+import org.junit.Assert.*
+
+/**
+ * Unit tests for the Helmut app.
+ * 
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
